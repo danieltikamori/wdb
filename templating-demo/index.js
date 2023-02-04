817477658319d3be1fd7e279596710b5229c57a2
danieltikamori <tikamori.itiro@gmail.com>
2023-02-04 22:17:51 +0900

    Add templating with EJS study project

---
+++ templating-demo/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home"); // as the the view and ejs was previously set, don't need to specify the complete path
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
+  res.render("cats", { allCats: cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
+app.get("/rand", (req, res) => {
+  const randomNum = Math.floor(Math.random() * 10 + 1);
+  res.render("random", { randomNum: randomNum });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
